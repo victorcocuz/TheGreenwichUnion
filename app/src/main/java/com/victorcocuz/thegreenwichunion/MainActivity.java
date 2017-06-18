5716e5949bd18c9d580f43252a05a63755d65f4f
victorcocuz <victor.cocuz@gmail.com>
2017-06-18 14:44:19 +0100

    Scroll View & External intents added

--- app/src/main/java/com/victorcocuz/thegreenwichunion/MainActivity.java
+++ app/src/main/java/com/victorcocuz/thegreenwichunion/MainActivity.java
@@ -1,8 +1,12 @@
 package com.victorcocuz.thegreenwichunion;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,24 @@ public class MainActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        LinearLayout addressLayout = (LinearLayout) findViewById(R.id.address);
+        addressLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.co.uk/maps?q=greenwich+union&um=1&ie=UTF-8&sa=X&ved=0ahUKEwjGuIavv8fUAhVEKlAKHdnRDaEQ_AUICigB"));
+                startActivity(mapIntent);
+            }
+        });
+
+        LinearLayout phoneLayout = (LinearLayout) findViewById(R.id.phone);
+        phoneLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: +44 020 8692 6258"));
+                startActivity(mapIntent);
+            }
+        });
     }
 }
 
